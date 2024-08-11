8ee5ac899685315c97f033a94181b594e2fb60bf
jeffersondavila <jeffersondavila16@gmail.com>
2024-08-11 16:58:07 -0600

    [ADD] Se agregan los modelos

---
+++ Web-Api/Models/Tarea.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Web_Api.Models
+{
+	public class Tarea
+	{
+		public Guid CodigoTarea { get; set; }
+		// [ForeignKey("CodigoCategoria")]
+		public Guid CodigoCategoria { get; set; }
+		// [Required]
+		// [MaxLength(200)]
+		public string Titulo { get; set; } = string.Empty;
+		public string Descripcion { get; set; } = string.Empty;
+		public Prioridad PrioridadTarea { get; set; }
+		public DateTime FechaCreacion { get; set; }
+		public virtual Categoria Categorias { get; set; }
+		[NotMapped]
+		public string Resumen { get; set; } = string.Empty;
+	}
+	public enum Prioridad
+	{
+		Baja,
+		Media,
+		Alta
+	}
+}
