cab51ffc6b8bcde6c7acd22d2c161c679642ed67
jeffersondavila <jeffersondavila16@gmail.com>
2024-08-11 15:27:51 -0600

    [ADD] Al crear un archivo de tipo middleware devuelve toda la plantilla solo para utilizarla en el program

---
+++ Web-Api/Middlewares/TimeMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Web_Api.Middlewares
+{
+	// You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+	public class TimeMiddleware
+	{
+		private readonly RequestDelegate _next;
+
+		public TimeMiddleware(RequestDelegate next)
+		{
+			_next = next;
+		}
+
+		public async Task Invoke(HttpContext httpContext)
+		{
+			await _next(httpContext);
+
+			if(httpContext.Request.Query.Any(p=> p.Key == "time"))
+			{
+				await httpContext.Response.WriteAsync(DateTime.Now.ToShortTimeString());
+			}
+		}
+	}
+
+	// Extension method used to add the middleware to the HTTP request pipeline.
+	public static class TimeMiddlewareExtensions
+	{
+		public static IApplicationBuilder UseTimeMiddleware(this IApplicationBuilder builder)
+		{
+			return builder.UseMiddleware<TimeMiddleware>();
+		}
+	}
+}
--- Web-Api/Program.cs
+++ Web-Api/Program.cs
@@ -1,3 +1,5 @@
+using Web_Api.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -19,6 +21,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseTimeMiddleware();
 
 app.MapControllers();
 
